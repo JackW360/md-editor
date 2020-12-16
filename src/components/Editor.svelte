a8a0812aced9da6b6b4f1a628073126fa9e7b875
Jack Whelan <jwhe0002@student.monash.edu>
2020-12-16 15:45:55 +1100

    Created basic block style markdown editor

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -4,22 +4,84 @@
     let text = '';
     let mdConverter = converter();
 
+    $: htmlStr = mdConverter.makeHtml(text);
+
+    let blocks = [];
+    let blockId = 0;
+
+    function addBlock(text){
+        let newBlock = {id: blockId++, html: mdConverter.makeHtml(text), text: text};
+        blocks = [...blocks, newBlock];
+    }
+
+    function pressEnter(key) {
+        if (key.key === "Enter"){
+            addBlock(text);
+            text = '';
+        }
+    }
+
 </script>
 
-<div>
-    <textarea bind:value={text} class="inline-div" placeholder="type some text..." cols=50 rows=10></textarea>
-    <div class="inline-div">Output:
-
-        <Viewer htmlStr={mdConverter.makeHtml(text)}></Viewer>
-    </div>
+<div class="container">
+    {#each blocks as block, index (block.id)}
+        <div class="block" contenteditable>
+            {@html block.html}
+        </div>
+    {/each}
+    <!-- <textarea bind:value={text} placeholder="type some text..." cols=50 rows=1 on:keypress={pressEnter}></textarea> -->
+    <input autofocus class="input" type="text" bind:value={text} on:keypress={pressEnter}>
 </div>
 
 <style>
-    .inline-div {
-        /* display: inline-block; */
-        float: left;
+    .block {
+        margin-top: 0px;
+        margin-bottom: 0px;
+        padding: 10px;
     }
+
+    .input {
+        width: 100%;
+        /* color: white; */
+        /* background-color: #1f1f1f; */
+        background-color: white;
+        border: none;
+    }
+
+    .block:hover, .input:hover {
+        /* background-color: #353535; */
+        background-color: rgb(240, 240, 240);
+    }
+
+    .block:focus, .input:focus {
+        background-color: #dfdfdf;
+    }
+
+    .container {
+        overflow-y: scroll;
+        margin: auto;
+        width: 80%;
+        max-width: 700px;
+        height: 100%;
+        max-height: 100%;
+        /* background-color: #1f1f1f; */
+        background-color: white;
+        border-radius: 5px;
+    }
+
     textarea {
         resize: none;
+    }
+
+    input:focus, textarea:focus {
+        border: none;
+        outline: none;
+    }
+
+    [contenteditable] {
+        /* color: blue; */
+        /* color: white; */
+        background-color: white;
+        outline: none;
     }
 </style>
