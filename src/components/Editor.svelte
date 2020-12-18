ac71e28fe365fde91f9fcb35c77f5123f35a486b
Jack Whelan <jwhe0002@student.monash.edu>
2020-12-18 20:37:52 +1100

    Multi-line syntax now works with shift+enter

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,97 +1,89 @@
 <script>
-    import { converter } from './showdown.js';
+    import { converter } from "./showdown.js";
 
-    let text = '';
     let mdConverter = converter();
-    let blocks = [{html: '', text: '', isFocused: true}];
+    let blocks = [{ html: "", text: "", isFocused: true }];
 
     function parseKeyPress(key, index) {
-        // console.log("key: ", key);
-        // console.log("index: ", index);
+        console.log("key: ", key);
+        console.log("index: ", index);
         if (key.key === "Enter") {
-            updateBlock(index);
-            addBlock(index);
-            text = '';
-        }
-        else if (key.key === "Backspace") {
+            // need to make sure that the cursor is at the end of the text input, otherwise should act differently
+            // probably need to act differently for shift + enter
+            if (!key.shiftKey) {
+                updateBlock(index);
+                addBlock(index);
+            }
+        } else if (key.key === "Backspace") {
             console.log("do something");
-            if (blocks[index].text === '') {
-                if (blocks.length) {
-                    console.log("blocks in parse key press for backspace", blocks);
-                    blocks.pop();
-                    let previousBlock = blocks[-1];
-                    removeFocus()
-                    previousBlock.isFocused = true;
+            if (blocks[index].text === "") {
+                if (index > 0) {
+                    let previousBlock = blocks[index - 1];
+                    blocks.splice(index);
                     blocks = blocks;
                     key.preventDefault();
+                    blockFocus(index - 1);
                 }
             }
+        } else if (key.key === "ArrowUp") {
+            // need to find a way of checking the position of the cursor
+            if (index > 0) {
+                blockFocus(index - 1);
+            }
+        } else if (key.key === "ArrowDown") {
+            // need to find a way of checking the position of the cursor
+            if (index < blocks.length - 1) {
+                blockFocus(index + 1);
+            }
+        } else if (key.key === "Shift") {
+            shiftHeld = true;
         }
     }
 
-    function updateBlock(index){
-        // blocks = blocks.map((obj) => {
-        //     obj.isFocused = false;
-        //     obj.html = mdConverter.makeHtml(obj.text);
-        //     return obj;
-        // })
+    function updateBlock(index) {
         blocks[index].html = mdConverter.makeHtml(blocks[index].text);
         blocks[index].isFocused = false;
         blocks = blocks;
     }
 
-    function addBlock(index){
-        let newBlock = {html: '', text: '', isFocused: true};
-        // blocks = [...blocks.slice(0, index + 1), newBlock, ...blocks.slice(index + 1)];
+    function addBlock(index) {
+        removeFocus();
+        let newBlock = { html: "", text: "", isFocused: true };
         blocks.splice(index + 1, 0, newBlock);
         blocks = blocks;
-        console.log("blocks after adding new block", blocks);
+        // blockFocus(index + 1);
     }
 
     function removeFocus() {
         blocks = blocks.map((obj) => {
             obj.isFocused = false;
             return obj;
-        })
+        });
     }
 
     function blockFocus(index) {
-        console.log('focussing block: ' + index);
+        console.log("focussing block: " + index);
         removeFocus();
         blocks[index].isFocused = true;
         blocks = blocks;
-        // document.getElementById("input_" + index).focus()
-        document.getElementById("input_" + index).focus()
-        // document.getElementById("input_" + index).click()
+        document.getElementById("input_" + index).focus();
     }
 
-    function blockBlur(id) {
-        console.log('blurring block: ' + id);
-        // let index = blocks.findIndex((obj) => {
-        //     return obj.id === id;
-        // })
-        // blocks[index].isFocused = true;
+    function autoResize() {
+        this.style.height = 'auto';
+        this.style.height = this.scrollHeight + 'px';
     }
-
 </script>
-
-<div class="container">
-    {#each blocks as block, index}
-        <input id={"input_" + index} class="block input" class:hidden={!block.isFocused} type="text" bind:value={block.text} on:keydown={ e => parseKeyPress(e, index)}>
-        <div id={"block_" + index} class="block" class:hidden={block.isFocused} on:click={e => blockFocus(index)} on:keydown={ e => parseKeyPress(e, index)}>
-            {@html block.html}
-        </div>
-    {/each}
-</div>
 
 <style>
     .block {
         margin-top: 0px;
         margin-bottom: 0px;
-        padding: 10px 15px;
+        padding: 5px 15px;
         z-index: 9;
         top: 0;
         left: 0;
+        min-height: 21px;
         /* width: 100%; */
         /* height: 20px; */
     }
@@ -110,12 +102,14 @@
         padding-left: 15px;
     }
 
-    .block:hover, .input:hover {
+    .block:hover,
+    .input:hover {
         /* background-color: #353535; */
         background-color: rgb(240, 240, 240);
     }
 
-    .block:focus, .input:focus {
+    .block:focus,
+    .input:focus {
         background-color: #dfdfdf;
     }
 
@@ -125,6 +119,7 @@
 
     .container {
         overflow-y: scroll;
+        /* overflow-y: visible; */
         position: relative;
         margin: auto;
         width: 80%;
@@ -139,9 +134,11 @@
 
     textarea {
         resize: none;
+        /* height: 30px; */
     }
 
-    input:focus, textarea:focus {
+    input:focus,
+    textarea:focus {
         border: none;
         outline: none;
     }
@@ -153,3 +150,34 @@
         outline: none;
     }
 </style>
+
+<div class="container">
+    {#each blocks as block, index}
+        <!-- <input
+            autofocus
+            id={'input_' + index}
+            class="block input"
+            class:hidden={!block.isFocused}
+            type="text"
+            bind:value={block.text}
+            on:keydown={(e) => parseKeyPress(e, index)}
+             /> -->
+        <textarea
+            autofocus
+            id={'input_' + index}
+            class="block input"
+            class:hidden={!block.isFocused}
+            type="text"
+            bind:value={block.text}
+            on:input={autoResize}
+            on:keydown={(e) => parseKeyPress(e, index)}/>
+        <div
+            id={'block_' + index}
+            class="block"
+            class:hidden={block.isFocused}
+            on:click={(e) => blockFocus(index)}
+            on:keydown={(e) => parseKeyPress(e, index)}>
+            {@html block.html}
+        </div>
+    {/each}
+</div>
